0929cb6c35ee8902e4a6946bb144f3388c44fd0a
jokamjohn <johnkagga@gmail.com>
2015-06-18 11:31:06 +0300

    Using the isEmpty method

--- Method/src/me/johnkagga/Main.java
+++ Method/src/me/johnkagga/Main.java
@@ -8,7 +8,17 @@ public class Main {
         This object initiates with a string object
          */
         PezDispenser dispenser = new PezDispenser("yoga");
-        System.out.printf("this is %s\n",dispenser.getCharacterName());
+        System.out.printf("this is %s\n", dispenser.getCharacterName());
         System.out.print(dispenser.isEmpty());
+
+        if (dispenser.isEmpty()){
+            System.out.println("\nIt is empty");
+        }
+        System.out.println("Loading content......");
+        //loading the dispenser
+        dispenser.load();
+        if (!dispenser.isEmpty()){
+            System.out.println("It is not empty");
+        }
     }
 }
