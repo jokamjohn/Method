56e271f583dd096ccecafb3a90538f9dc30f6032
jokamjohn <johnkagga@gmail.com>
2015-06-18 10:47:02 +0300

    Added constructor

--- Method/src/me/johnkagga/PezDispenser.java
+++ Method/src/me/johnkagga/PezDispenser.java
@@ -4,4 +4,14 @@ package me.johnkagga;
  * Created by JohnKagga on 6/18/2015.
  */
 public class PezDispenser {
+    public static final int MAX_PREZ = 12;
+    private String mCharacterName = "Yoga";
+
+    public PezDispenser(String characterName) {
+        mCharacterName = characterName;
+    }
+
+    public String getCharacterName() {
+        return mCharacterName;
+    }
 }
