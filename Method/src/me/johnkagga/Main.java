de73aa4d1aaf8e36d32a6300f929d1c46d6eddc0
jokamjohn <johnkagga@gmail.com>
2015-06-18 10:55:02 +0300

    Added getter and documentation

--- Method/src/me/johnkagga/Main.java
+++ Method/src/me/johnkagga/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        /*
+        This object initiates with a string object
+         */
+        PezDispenser dispenser = new PezDispenser("yoga");
+        System.out.printf("this is %s",dispenser.getCharacterName());
     }
 }
--- Method/src/me/johnkagga/PezDispenser.java
+++ Method/src/me/johnkagga/PezDispenser.java
@@ -4,13 +4,25 @@ package me.johnkagga;
  * Created by JohnKagga on 6/18/2015.
  */
 public class PezDispenser {
+    /*
+    We have created a PezDispenser (object)
+    its maximum input is 12, that is why it a constant
+    mcharacter is the type of the dispenser
+     */
     public static final int MAX_PREZ = 12;
     private String mCharacterName = "Yoga";
 
+    /*
+    The constructor takes in the desired type of prez.
+    It enables construction when the object is initiated
+     */
     public PezDispenser(String characterName) {
         mCharacterName = characterName;
     }
 
+    /*
+    This method returns the name of the prez character
+     */
     public String getCharacterName() {
         return mCharacterName;
     }
